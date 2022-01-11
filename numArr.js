1e6ac5c82f15abce094fb95bb248d9d30dfd8803
N00bG1rl <marttila.helen@gmail.com>
2022-01-11 19:19:34 +0200

    Initial Commit

---
+++ numArr.js
@@ -0,0 +1,18 @@
+const numbers = ['5688848', '34593834', '4902348020', '56578922']
+const resultArr = []
+
+numbers.map(num => {
+  const count = num.split('').reduce((prev, curr) => {
+    prev[curr] = (prev[curr] || 0) + 1
+
+    return prev
+  }, {})
+
+  for (let value of Object.values(count)) {
+    if ([3, 4].includes(value)) {
+      resultArr.push(num)
+    }
+  }
+})
+
+console.log(resultArr) // => [ '5688848', '34593834', '4902348020' ]
